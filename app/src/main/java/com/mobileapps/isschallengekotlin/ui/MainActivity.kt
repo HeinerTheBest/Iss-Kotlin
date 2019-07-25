b8a1b53f7144cd9ba5016e0769fc2f63b4112a97
Admin <heinerbauza@gmail.com>
2019-07-24 21:07:35 -0400

    first part

--- app/src/main/java/com/mobileapps/isschallengekotlin/ui/MainActivity.kt
+++ app/src/main/java/com/mobileapps/isschallengekotlin/ui/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.mobileapps.isschallengekotlin.ui
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Adapter
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.mobileapps.isschallengekotlin.R
 import com.mobileapps.isschallengekotlin.adapters.IssAdapter
 import com.mobileapps.isschallengekotlin.models.IssResponse
@@ -17,15 +24,54 @@ class MainActivity : AppCompatActivity(), MainViewInterface  {
     val TAG = "MainActivity"
     lateinit var adapter: IssAdapter
     lateinit var mainPresenter : MainPresenter
+    lateinit var fusedLocationClient: FusedLocationProviderClient
+    lateinit var lat : String
+    lateinit var long: String
+
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setupMVP()
-        setupViews()
-        getResponse()
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+
+       startPermission()
+
+
+    }
+
+    private fun startPermission() {
+        if (checkPermission(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            fusedLocationClient.lastLocation?.
+                addOnSuccessListener(this
+                ) { location ->
+                    // Got last known location. In some rare
+                    // situations this can be null.
+                    if(location == null) {
+                        // TODO, handle it
+                    } else location.apply {
+                        lat = location.latitude.toString()
+                        long = location.longitude.toString()
+                        setupMVP()
+                        setupViews()
+                        getResponse()
+                    }
+                }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray)
+    {
+        when (requestCode) {
+            PERMISSION_ID -> {
+                startPermission()
+            }
+        }
     }
 
     private fun getResponse()
@@ -39,8 +85,8 @@ class MainActivity : AppCompatActivity(), MainViewInterface  {
     }
 
     private fun setupMVP()
-    {
-        mainPresenter = MainPresenter(this,"33.90","-84.47")
+    {//"33.90","-84.47"
+        mainPresenter = MainPresenter(this,lat,long)
     }
 
 
@@ -56,6 +102,35 @@ class MainActivity : AppCompatActivity(), MainViewInterface  {
 
     override fun displayError(s: String) {
         Toast.makeText(this, s, Toast.LENGTH_SHORT).show()
+    }
+
+    val PERMISSION_ID = 42
+    private fun checkPermission(vararg perm:String) : Boolean {
+        val havePermissions = perm.toList().all {
+            ContextCompat.checkSelfPermission(this,it) ==
+                    PackageManager.PERMISSION_GRANTED
+        }
+        if (!havePermissions) {
+            if(perm.toList().any {
+                    ActivityCompat.
+                        shouldShowRequestPermissionRationale(this, it)}
+            ) {
+                val dialog = AlertDialog.Builder(this)
+                    .setTitle("Permission")
+                    .setMessage("Permission needed!")
+                    .setPositiveButton("OK") { id, v ->
+                        ActivityCompat.requestPermissions(
+                            this, perm, PERMISSION_ID)
+                    }
+                    .setNegativeButton("No", {id, v -> })
+                    .create()
+                dialog.show()
+            } else {
+                ActivityCompat.requestPermissions(this, perm, PERMISSION_ID)
+            }
+            return false
+        }
+        return true
     }
 
 }
